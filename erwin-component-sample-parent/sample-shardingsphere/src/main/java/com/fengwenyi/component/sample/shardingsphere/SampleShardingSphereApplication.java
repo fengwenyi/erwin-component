91908ef6c430171874ef5d7d0e6135a768baa50d
ErwinFeng <xfsy_2015@163.com>
2025-01-05 14:12:16 +0800

    ShardingSphere sample

---
+++ erwin-component-sample-parent/sample-shardingsphere/src/main/java/com/fengwenyi/component/sample/shardingsphere/SampleShardingSphereApplication.java
@@ -0,0 +1,16 @@
+package com.fengwenyi.component.sample.shardingsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2025-01-04
+ */
+@SpringBootApplication
+public class SampleShardingSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleShardingSphereApplication.class, args);
+    }
+}
