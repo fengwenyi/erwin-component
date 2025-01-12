54017e7bdd24a15173b03c31fd86ee045ab3ed12
ErwinFeng <xfsy_2015@163.com>
2025-01-12 22:48:38 +0800

    build: version upgrade
    spring-boot 3.4.1
    mybatis-plus 3.5.10
    mybatis-plus 分页插件

--- erwin-component-common-parent/erwin-component-common-mybatis-plus/src/main/java/com/fengwenyi/erwin/component/common/mybatis_plus/config/MyBatisPlusComponentConfig.java
+++ erwin-component-common-parent/erwin-component-common-mybatis-plus/src/main/java/com/fengwenyi/erwin/component/common/mybatis_plus/config/MyBatisPlusComponentConfig.java
@@ -1,7 +1,10 @@
 package com.fengwenyi.erwin.component.common.mybatis_plus.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.fengwenyi.erwin.component.common.util.SnowFlakeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -10,18 +13,26 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * @author <a href="https://fengwenyi.com">Erwin Feng</a>
- * @since 2024-01-03
+ * @since 2023-07-25
  */
-@Configuration
 @Slf4j
-public class MyBatisPlusSnowFlakeConfig {
+@Configuration
+public class MyBatisPlusComponentConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        final MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 分页插件
+        return interceptor;
+    }
 
     @Bean
     @ConditionalOnMissingBean
     public IdentifierGenerator identifierGenerator()  {
         long workId = SnowFlakeUtils.getWorkId();
         long dataCenterId = SnowFlakeUtils.getDataCenterId();
-        log.info("MyBatisPlusSnowFlakeConfig, identifierGenerator, workId: [{}], dataCenterId: [{}]", workId, dataCenterId);
+        log.info("MyBatisPlusComponentConfig, identifierGenerator, workId: [{}], dataCenterId: [{}]", workId, dataCenterId);
         return new DefaultIdentifierGenerator(workId, dataCenterId);
     }
 
